88150e18f5514668ee4b3a808d80682d9a1a6597
P219-C <19741043+P219-C@users.noreply.github.com>
2022-01-15 22:51:26 +0800

    Added comments to the js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,26 @@
+// For Output format check the original source https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
-console.log(queryUrl)
+// console.log(queryUrl)   // Uncomment to print the URL in console
 
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
 
 d3.json(queryUrl).then(function(data) {
-    // Once we get a response, send the data.features object to the createFeatures function
-    console.log(data);
+    // console.log(data);   // Uncomment to print the full dataset in console
+
+
+    // ************ Code to test the dataset. It prints the location name of the earthquake"
     var fullCountries=[];
     data.features.forEach(earthquake =>{
         // console.log(earthquake.properties.place);
         var location = earthquake.properties.place.split(",");
         fullCountries.push(location.pop());
-
     });
-
     var uniqueCountries = fullCountries.filter(onlyUnique);
     console.log(uniqueCountries);
+    // *****************************************************************
+
+
 });
