cc3e3703f23f4b3646464c0508c35ecd80797acb
P219-C <19741043+P219-C@users.noreply.github.com>
2022-01-15 22:45:34 +0800

    index.html initialized and geojson file fetched

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,21 @@
-// code here
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+console.log(queryUrl)
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+d3.json(queryUrl).then(function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    console.log(data);
+    var fullCountries=[];
+    data.features.forEach(earthquake =>{
+        // console.log(earthquake.properties.place);
+        var location = earthquake.properties.place.split(",");
+        fullCountries.push(location.pop());
+
+    });
+
+    var uniqueCountries = fullCountries.filter(onlyUnique);
+    console.log(uniqueCountries);
+});
