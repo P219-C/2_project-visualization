1fbd1998270c4c40e7a7e3eade6c2571517cb4e4
Mona Sidhu <mona_leo2@yahoo.com>
2022-01-24 21:05:40 +0800

    updated barplot

--- static/js/barplot.js
+++ static/js/barplot.js
@@ -1,28 +1,28 @@
 d3.json("/API/countries_earthquakes").then(function(data) {
+const Country = [...new Set(data.map(item => item.CountryName))];
 
-  var Country = ["South Korea", "Canada", "United Kingdom", "Netherlands", "Italy",
-  "France", "Japan", "United States", "China", "Germany"];
+  console.log(Country)
   
-  var Earthquakes = [1500,2500,1300,1450,650,700,850,1200,900,850];
+  // var Earthquakes = data.dataset.Magnitude;
   
-  // create the Trace
-  var trace1 = {
-    x: Earthquakes,
-    y : Country,
-    type: "bar"
-  }
-  // Create the data array for the plot
-  var data = [trace1];
+  // // create the Trace
+  // var trace1 = {
+  //   x: Earthquakes,
+  //   y : Country,
+  //   type: "bar"
+  // }
+  // // Create the data array for the plot
+  // var data = [trace1];
   
-  // Define the plot layout
-  var layout = {
-    title: "Country vs Earthquake",
-    xaxis: { title: " Number of Earthquakes" },
-    yaxis: { title: "Country " }
-  };
+  // // Define the plot layout
+  // var layout = {
+  //   title: "Country vs Earthquake",
+  //   xaxis: { title: " Number of Earthquakes" },
+  //   yaxis: { title: "Country " }
+  // };
   
-  // Plot the chart to a div tag with id "bar-plot"
-  Plotly.newPlot("bar-plot", data, layout);
+  // // Plot the chart to a div tag with id "bar-plot"
+  // Plotly.newPlot("bar-plot", data, layout);
 
 
 
